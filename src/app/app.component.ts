9b3d53bdad02b3a5639cd22087bf7e5569406053
skrikl <sergey.kriklivyy@gmail.com>
2020-07-16 22:49:00 +0300

    [new] - added: assets; initial components layout; CDN bootstrap/fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { data } from '../assets/data/model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'social-systems';
+  data = [
+    {
+      name: 'Андрей Иванович',
+      type: 'Пост Инстаграм',
+      avatar: '../assets/img/avatar1.jpg',
+      services: [
+        {
+          name: 'Лайки',
+          icon: '../assets/icons/like.svg',
+          currentValue: 10000,
+          total: 20000
+        },
+        {
+          name: 'Репосты',
+          icon: '../assets/icons/repost.svg',
+          currentValue: 10000,
+          total: 15000
+        }
+      ]
+    },
+    {
+      name: 'Дмитрий Головаев',
+      type: 'Аккаунт Инстаграм',
+      avatar: '../assets/img/avatar2.jpg',
+      services: [
+        {
+          name: 'Подписчики',
+          icon: '../assets/icons/subscribe.svg',
+          currentValue: 0,
+          total: 20000
+        }
+      ]
+    }
+  ];
+  ngOnInit(): void {
+    console.log('Initial data', this.data);
+  }
 }
---
+++ src/assets/data/model.ts
@@ -0,0 +1,29 @@
+export const data = [
+    {
+        name: 'Андрей Иванович',
+        type: 'Пост Инстаграм',
+        services: [
+            {
+                name: 'Лайки',
+                currentValue: 10000,
+                total: 20000
+            },
+            {
+                name: 'Репосты',
+                currentValue: 10000,
+                total: 15000
+            }
+        ]
+    },
+    {
+        name: 'Дмитрий Головаев',
+        type: 'Аккаунт Инстаграм',
+        services: [
+            {
+                name: 'Подписчики',
+                currentValue: 0,
+                total: 20000
+            }
+        ]
+    }
+];
